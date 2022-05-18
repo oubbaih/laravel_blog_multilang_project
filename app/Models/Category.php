c5af26567b99adbf8e4545a53dac19c17cd851d8
lahcen oubbaih <eoubbaih@gmail.com>
2022-05-18 14:56:17 +0100

    migrations setup

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -3,9 +3,14 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Astrotomic\Translatable\Contracts\Translatable as TranslatableContract;
+use Astrotomic\Translatable\Translatable;
 use Illuminate\Database\Eloquent\Model;
 
-class Category extends Model
+class Category extends Model implements TranslatableContract
 {
+    use Translatable;
     use HasFactory;
+    public $translatedAttributes = ['title', 'content'];
+    protected $fillable = ['parent', 'image'];
 }
